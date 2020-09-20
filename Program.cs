beb98fc81a50ff2e697d21a20cf16c490cefee86
seoknyang <55523889+seoknyang@users.noreply.github.com>
2020-09-20 11:39:17 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            Console.Write("더하기:");
+            Console.WriteLine(a+b);
+            Console.Write("빼기:");
+            Console.WriteLine(a - b);
+            Console.Write("곱하기:");
+            Console.WriteLine(a * b);
+            Console.Write("나누기");
+            Console.WriteLine(a / (float)b);
+        }
+
+    }
+}
